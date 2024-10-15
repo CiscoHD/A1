b7f3982fb06df32da9c3801eb34b28b1848e86f6
Francisco Uriel Pineda Hernández <323331535@alumno.enp.unam.mx>
2024-10-15 11:18:23 -0600

    Try 2

--- A.bat
+++ A.bat
@@ -3,12 +3,12 @@ cd \
 md \Users\Public\Documents\Word
 md \Users\Public\Music\Spotify
 
-move \A\ETECprac.txt \Users\Public\Documents\Word
-move \A\ETECLibre.txt \Users\Public\Music\Spotify
-move \A\Practica2Libre.docx 
+move \A1\ETECprac.txt \Users\Public\Documents\Word
+move \A1\ETECLibre.txt \Users\Public\Music\Spotify
+move \A1\Practica2Libre.docx 
 
-move \A\G61B \
-move \A\Grupo61D \
+move \A1\G61B \
+move \A1\Grupo61D \
 
 attrib +r \Users\Public\Documents\Word\ETECprac.txt
 attrib +r \Users\Public\Music\Spotify\ETECLibre.txt
@@ -16,4 +16,4 @@ attrib +r \Users\Public\Music\Spotify\ETECLibre.txt
 path ;
 cls
 
-rmdir /s /q \A
+rmdir /s /q \A1
